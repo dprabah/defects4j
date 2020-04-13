0b96c50efb4375c5daaf97b032cd8d826b3b2f93
prabah <prabaharan.durairaj1@gmail.com>
2020-04-13 22:26:57 +0200

    enh: automated script can now handle break and continue

--- framework/bin/run_defects4j.sh
+++ framework/bin/run_defects4j.sh
@@ -13,9 +13,17 @@ for i in $(seq $start_bug $end_bug);
 do
     dir_name="/home/ubuntu/tmp/"$bug_code"_"$i"_fixed"
     bug_id=$i"f"
-    mkdir -p $dir_name
-    defects4j checkout -p $bug_code -v $bug_id -w $dir_name
-    cd $dir_name
-    pwd
-    defects4j checked
+
+    if [ -d "$dir_name" ]; then
+      echo "dir seems to be exist already"
+      cd $dir_name
+      defects4j checked
+    else
+      echo "dir not found, creating"
+      mkdir -p $dir_name
+      defects4j checkout -p $bug_code -v $bug_id -w $dir_name
+      cd $dir_name
+      pwd
+      defects4j checked
+    fi
 done
