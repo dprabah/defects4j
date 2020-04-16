ba4f8d84d301977c31028f6d42226454cc32d943
prabah <prabaharan.durairaj1@gmail.com>
2020-04-17 01:05:24 +0200

    feature: defects4j checked -c coverage computes coverage for relevant and defects4j checked -c coverage -a all computes coverage for all classes

--- framework/util/test_class_modifier/coverage_calculator/statement_score.py
+++ framework/util/test_class_modifier/coverage_calculator/statement_score.py
@@ -5,13 +5,22 @@ import json
 def compute(xml_file_path, json_store_path):
     xml_doc = minidom.parse(xml_file_path)
     classes = xml_doc.getElementsByTagName('class')
-    numbers = {}
+
+    output = {}
     for current_class in classes:
-        lines = current_class.getElementsByTagName('line')
-        line_nr = []
-        for line in lines:
-            line_nr.append(int(line.attributes['number'].value))
-        numbers[current_class.attributes['name'].value] = sorted(list(set(line_nr)))
+        class_name = current_class.attributes['name'].value
+        methods = current_class.getElementsByTagName('method')
+        numbers = {}
+        for current_method in methods:
+            method_name = current_method.attributes['name'].value
+            lines = current_method.getElementsByTagName('line')
+            line_nr = []
+            for line in lines:
+                if int(line.attributes['hits'].value) > 0:
+                    line_nr.append(int(line.attributes['number'].value))
+            numbers[method_name] = sorted(list(set(line_nr)))
+        output[class_name] = numbers
+            # numbers[class_name].append(numbers[method_name])
 
     with open(json_store_path+"/line_coverage.json", 'w') as fp:
-        json.dump(numbers, fp)
+        json.dump(output, fp)
