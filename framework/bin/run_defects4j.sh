701756c01febd7771ebaaaec2b1b1141d1fbc79e
prabah <prabaharan.durairaj1@gmail.com>
2020-05-01 11:51:07 +0200

    enh: adding new param type

--- framework/bin/run_defects4j.sh
+++ framework/bin/run_defects4j.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-while getopts "s:e:i:" opt
+while getopts "s:e:i:t:" opt
 do
 case "$opt" in
 s ) start_bug="$OPTARG" ;;
 e ) end_bug="$OPTARG" ;;
 i ) bug_code="$OPTARG" ;;
+t ) type_to_run="$OPTARG" ;;
 esac
 done
 
@@ -18,12 +19,12 @@ do
     if [ -d "$dir_name" ]; then
       echo "dir seems to be exist already"
       cd "$dir_name"
-      defects4j checked -b both &>>$log_file
+      defects4j checked "$type_to_run" &>>$log_file
     else
       echo "dir not found, creating"
       mkdir -p "$dir_name"
       defects4j checkout -p "$bug_code" -v "$bug_id" -w "$dir_name"
       cd "$dir_name"
-      defects4j checked -b both &>>$log_file
+      defects4j checked "$type_to_run" &>>$log_file
     fi
 done
