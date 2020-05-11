d9cdb79ba86b3812b6bbfe17c0ed4375c1d0fd9e
prabah <prabaharan.durairaj1@gmail.com>
2020-05-11 11:46:53 +0200

    bug: fixed a bug in statement score calculator

--- framework/util/test_class_modifier/coverage_calculator/statement_score.py
+++ framework/util/test_class_modifier/coverage_calculator/statement_score.py
@@ -1,12 +1,10 @@
 from os import path
 from xml.dom import minidom
 import json
-import sys
 import re
 
 
 def compute(xml_file_path, json_store_path, executed_method):
-    # executed_method = executed_method.split("::")[1]
     xml_doc = minidom.parse(xml_file_path)
     classes = xml_doc.getElementsByTagName('class')
     readable_file_path = json_store_path + "/line_coverage.json"
@@ -14,26 +12,20 @@ def compute(xml_file_path, json_store_path, executed_method):
     if path.exists(readable_file_path):
         with open(readable_file_path) as fp:
             output = json.load(fp)
-    numbers = {}
     for current_class in classes:
         class_name = current_class.attributes['name'].value
-        methods = current_class.getElementsByTagName('method')
+        lines = current_class.getElementsByTagName('line')
         line_nr = []
-        for current_method in methods:
-            # method_name = current_method.attributes['name'].value
-            method_name = str(executed_method)
-            lines = current_method.getElementsByTagName('line')
-            for line in lines:
-                if int(line.attributes['hits'].value) > 0:
-                    line_nr.append(int(line.attributes['number'].value))
-            if line_nr.__len__() > 0:
-                numbers[method_name] = sorted(list(set(line_nr)))
-        if numbers.__len__() > 0:
+        numbers = {}
+        for line in lines:
+            if int(line.attributes['hits'].value) > 0:
+                line_nr.append(int(line.attributes['number'].value))
+        if line_nr.__len__() > 0:
+            numbers[executed_method] = sorted(list(set(line_nr)))
             try:
                 output[class_name].update(numbers)
             except KeyError:
                 output[class_name] = numbers
-            # numbers[class_name].append(numbers[method_name])
 
     with open(readable_file_path, 'w') as fp:
         json.dump(output, fp)
