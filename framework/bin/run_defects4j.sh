8ac1c605831629227ff8e8c2265c0cad9490985f
prabah <prabaharan.durairaj1@gmail.com>
2020-12-20 14:12:52 +0100

    enh: support for parallelization

--- framework/bin/run_defects4j.sh
+++ framework/bin/run_defects4j.sh
@@ -11,6 +11,7 @@ u ) user_name="$OPTARG" ;;
 esac
 done
 
+# we checkout all the projects initially
 for i in $(seq "$start_bug" "$end_bug");
 do
     dir_name="/home/"$user_name"/tmp/"$bug_code"_"$i"_fixed"
@@ -21,14 +22,39 @@ do
     log_file="$log_file_dir/running.log"
 
     if [ -d "$dir_name" ]; then
-      echo "dir seems to be exist already"
+      echo "bug ${bug_code}-${bug_id} seems to be cloned already, skipping checkout"
+    else
+      echo "clonning the bug ${bug_code}-${bug_id} into local."
+      cd
+      mkdir -p "$dir_name"
+      defects4j checkout -p "$bug_code" -v "$bug_id" -w "$dir_name"
+      cd "$dir_name"
+      if [ ! -d "$log_file_dir" ]; then
+        mkdir -p "$log_file_dir"
+      fi
+    fi
+done
+echo "Clonning completed."
+echo "Starting computation."
+
+for i in $(seq "$start_bug" "$end_bug");
+do
+    dir_name="/home/"$user_name"/tmp/"$bug_code"_"$i"_fixed"
+    bug_id=$i"f"
+    defects4j_path=$(which defects4j)
+    defects4j_path=${defects4j_path::-24}
+    log_file_dir=$defects4j_path"/framework/projects/$bug_code/trace_files/"$bug_id
+    log_file="$log_file_dir/running.log"
+
+    if [ -d "$dir_name" ]; then
+      echo "bug ${bug_code}-${bug_id} seems to be cloned already, skipping checkout"
       cd "$dir_name"
       if [ ! -d "$log_file_dir" ]; then
         mkdir -p "$log_file_dir"
       fi
       defects4j checked "$type_to_run" &>>"$log_file"
     else
-      echo "dir not found, creating"
+      echo "clonning the bug ${bug_code}-${bug_id} into local."
       cd
       mkdir -p "$dir_name"
       defects4j checkout -p "$bug_code" -v "$bug_id" -w "$dir_name"
