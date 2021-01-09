8a5a1e3d75a618bd80eb1b2e01841737c13a197b
prabah <prabaharan.durairaj1@gmail.com>
2021-01-09 19:13:09 +0100

    enh: moved checked statements computing logic into slice.sh file

--- framework/util/test_class_modifier/file_editor/convert_checked_score_json.py
+++ framework/util/test_class_modifier/file_editor/convert_checked_score_json.py
@@ -1,5 +1,20 @@
 import json
 import os.path
+from os import path
+
+
+def read_json_file(file_path):
+    if not path.exists(file_path):
+        # print("File not exist!" + file_path)
+        output = {}
+    else:
+        with open(file_path, encoding="ISO-8859-1") as fp:
+            output = json.load(fp)
+
+    for key in output:
+        for slave in output[key]:
+            output[key][slave] = sorted(list(set(output[key][slave])))
+    return output
 
 
 def read_format_file(file_path, to_save):
@@ -23,3 +38,36 @@ def read_format_file(file_path, to_save):
 
     with open(to_save, 'w') as fp:
         json.dump(final_output, fp)
+
+
+master_file = dict()
+
+
+def remove_empty_from_dict(d):
+    if type(d) is dict:
+        return dict((k, remove_empty_from_dict(v)) for k, v in d.items() if v and remove_empty_from_dict(v))
+    elif type(d) is list:
+        return [remove_empty_from_dict(v) for v in d if v and remove_empty_from_dict(v)]
+    else:
+        return d
+
+
+def update_to_huge_file(folder_path, to_save):
+    global master_file
+    for root, dirs, files in os.walk(os.path.abspath(folder_path)):
+        for file in files:
+            json_file = os.path.join(root, file)
+            if json_file.endswith(".slice.output.result.json"):
+                print("Computing for -> " + str(json_file))
+                if master_file.__len__() == 0:
+                    master_file = read_json_file(json_file)
+                    continue
+                compute(read_json_file(json_file))
+
+    with open(to_save, 'w') as fp:
+        json.dump(remove_empty_from_dict(master_file), fp)
+
+
+def compute(json_file_to_append):
+    for key in master_file:
+        master_file[key].update(json_file_to_append[key])
--- framework/util/test_class_modifier/update_checked_json.py
+++ framework/util/test_class_modifier/update_checked_json.py
@@ -4,11 +4,12 @@ from file_editor import convert_checked_score_json
 
 
 def main():
-    arguments = len(sys.argv) - 2
+    arguments = len(sys.argv) - 1
     if arguments != 1:
         print("please verify arguments, size mismatch")
         sys.exit(2)
-    convert_checked_score_json.read_format_file(sys.argv[1], sys.argv[2])
+    folder_path = sys.argv[1].split("checked_coverage.json")[0]
+    convert_checked_score_json.update_to_huge_file(folder_path, sys.argv[1])
 
 
 if __name__ == "__main__":
