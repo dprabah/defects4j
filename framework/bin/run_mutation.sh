1e65a4ac5644fcd8027d5ba40f25a73546883a73
prabah <prabaharan.durairaj1@gmail.com>
2021-01-30 05:31:59 +0100

    enh: added script for running

---
+++ framework/bin/run_mutation.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+while getopts "s:e:i:t:u:" opt
+do
+case "$opt" in
+s ) start_bug="$OPTARG" ;;
+e ) end_bug="$OPTARG" ;;
+i ) bug_code="$OPTARG" ;;
+t ) type_to_run="$OPTARG" ;;
+u ) user_name="$OPTARG" ;;
+esac
+done
+
+# we checkout all the projects initially
+echo "STARTING TO CHECKOUT THE PROJECTS"
+
+for i in $(seq "$start_bug" "$end_bug");
+do
+    dir_name="/home/"$user_name"/tmp/"$bug_code"_"$i"_fixed"
+    # dir_name="/scratch/gambi/checked-coverage/tmp/"$bug_code"_"$i"_fixed"
+
+    bug_id=$i"f"
+    defects4j_path=$(which defects4j)
+    defects4j_path=${defects4j_path::-24}
+    log_file_dir=$defects4j_path"/framework/projects/$bug_code/trace_files/"$bug_id
+    log_file="$log_file_dir/checking-out.log"
+
+    if [ -d "$dir_name" ]; then
+      echo "bug ${bug_code}-${bug_id} seems to be cloned already, skipping checkout"
+    else
+      echo "clonning the bug ${bug_code}-${bug_id} into local."
+      cd
+      mkdir -p "$dir_name"
+      defects4j checkout -p "$bug_code" -v "$bug_id" -w "$dir_name"
+      cd "$dir_name"
+      if [ ! -d "$log_file_dir" ]; then
+        mkdir -p "$log_file_dir"
+      fi
+    fi
+done
+echo "Clonning completed."
+
+echo "Starting computation."
+for i in $(seq "$start_bug" "$end_bug");
+do
+    dir_name="/home/"$user_name"/tmp/"$bug_code"_"$i"_fixed"
+    # dir_name="/scratch/gambi/checked-coverage/tmp/"$bug_code"_"$i"_fixed"
+
+    bug_id=$i"f"
+    defects4j_path=$(which defects4j)
+    defects4j_path=${defects4j_path::-24}
+    log_file_dir=$defects4j_path"/framework/projects/$bug_code/trace_files/"$bug_id
+    log_file="$log_file_dir/setup-running.log"
+
+    echo ">> Logging to ${log_file}"
+
+    if [ -d "$dir_name" ]; then
+      echo "bug ${bug_code}-${bug_id} seems to be cloned already, skipping checkout"
+      cd "$dir_name"
+      if [ ! -d "$log_file_dir" ]; then
+        mkdir -p "$log_file_dir"
+      fi
+      defects4j mutation 2>&1 | tee -a "$log_file"
+    else
+      echo "Project is missing. First check out the project"
+    fi
+done
